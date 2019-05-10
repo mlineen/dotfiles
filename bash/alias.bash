35e91e7eddb7e53994e683092886c9b96747e48e
Matthew Lineen <matthew@lineen.com>
2019-05-10 14:33:41 -0700

    ADD gcs alias

--- bash/alias.bash
+++ bash/alias.bash
@@ -1,6 +1,7 @@
 alias e=emacs
 alias g=hub
 alias gcd="hub checkout develop"
+alias gcs="hub checkout staging"
 alias gcm="hub checkout master"
 alias gp="hub pull"
 alias prune="git branch -l | grep -v master | grep -v develop | xargs git branch -d && git remote prune origin"
