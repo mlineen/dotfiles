61e5dd28d534521e43135a27cb6a93d82134c3b7
Matthew Lineen <matthew@lineen.com>
2019-10-09 09:54:36 -0500

    UPDATE aliases

--- bash/alias.bash
+++ bash/alias.bash
@@ -1,7 +1,8 @@
+alias duh="du -d 1 -h | sort -h"
 alias e=emacs
 alias g=hub
 alias gcd="hub checkout develop"
-alias gcs="hub checkout staging"
 alias gcm="hub checkout master"
+alias gcs="hub checkout staging"
 alias gp="hub pull"
 alias prune="git branch -l | grep -v master | grep -v develop | xargs git branch -d && git remote prune origin"
